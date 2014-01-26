c430eeff5108f02e98af315d6443e4f6e6bbfb27
Great Leaper <keith.a.weightman@gmail.com>
2014-01-26 17:26:47 -0500

    oops - didn't put it in my app.

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,6 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-config.assets.initialize_on_precompile = false
-
 module Amms
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -60,5 +58,9 @@ module Amms
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # This was added cause of this: https://devcenter.heroku.com/articles/getting-started-with-rails3#write-your-app
+    config.assets.initialize_on_precompile = false
+
   end
 end
